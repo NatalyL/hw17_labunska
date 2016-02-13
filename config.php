90017eda3804c44c0dfc2e6b5373a9e75af4c755
NatalyL <labunskaya.nataly@gmail.com>
2016-02-13 15:22:37 +0200

    php was changed in config

--- config.php
+++ config.php
@@ -1,66 +1,60 @@
 <?php
 
-return array(
+return [
 
-    'counter' => array(
-        array(
-            'image_path' => 'images/jedi.png',
-        ),
-        array(
-            'image_path' => 'images/droids.png',
-        ),
-        array(
-            'image_path' => 'images/battles.png',
-        ),
-    ),
+    'counter' => [
+        ['image_path' => 'images/jedi.png'],
+        ['image_path' => 'images/droids.png'],
+        ['image_path' => 'images/battles.png'],
+    ],
 
-    'opinions' => array(
-        array(
+    'opinions' => [
+        [
             'image_path' => 'images/opinions_item1.png',
             'title' => '“Lorem ipsum dolor sit amet, consectetur adipisicing elit sed”',
             'author' => 'Yoda, ',
-        ),
-        array(
+        ],
+        [
             'image_path' => 'images/opinions_item1.png',
             'title' => '“Lorem ipsum dolor sit amet, consectetur adipisicing elit sed”',
             'author' => 'MACE WINDU, ',
-        ),
-        array(
+        ],
+        [
             'image_path' => 'images/opinions_item1.png',
             'title' => '“Lorem ipsum dolor sit amet, consectetur adipisicing elit sed”',
             'author' => 'Obi Wan, ',
-        ),
-        array(
+        ],
+        [
             'image_path' => 'images/opinions_item1.png',
             'title' => '“Lorem ipsum dolor sit amet, consectetur adipisicing elit sed”',
             'author' => 'A. Skywalker, ',
-        ),
-    ),
+        ],
+    ],
 
-    'carousel' => array (
-        array(
+    'carousel' => [
+        [
             'url' => '#',
             'image_path' => 'images/news_item1.png',
             'title' => 'Lorem ipsum dolor',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor',
-        ),
-        array(
+        ],
+        [
             'url' => '#',
             'image_path' => 'images/news_item1.png',
             'title' => 'Lorem ipsum dolor',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor',
-        ),
-        array(
+        ],
+        [
             'url' => '#',
             'image_path' => 'images/news_item1.png',
             'title' => 'Lorem ipsum dolor',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor',
-        ),
-        array(
+        ],
+        [
             'url' => '#',
             'image_path' => 'images/news_item1.png',
             'title' => 'Lorem ipsum dolor',
             'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor',
-        ),
-    ),
-);
+        ],
+    ],
+];
